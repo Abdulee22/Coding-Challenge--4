b8e59c33b45ef5bc58ee120141b45832cafbd8cd
Abdulhakim <Abdulthe383@gmail.com>
2025-02-02 23:11:48 -0500

    Task 2 Sales Report

--- cc_4.js
+++ cc_4.js
@@ -4,3 +4,9 @@ let finalAmont = purchaseAmount;
 if (purchaseAmount> 100) { finalAmount = purchaseAmount*.9};
 console.log ("discount applied");
 
+// Task 2 Sales Report Loop //
+let sales = [120,85,200,150,90];
+let totalSales = 0;
+for (let i = 0; i < sales.length; i++)
+{totalSales += sales[i]}
+console.log ('Total Sales: $$ {totalSales}');
