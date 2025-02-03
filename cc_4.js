08b4d1027ae70950e2c6616a4b54555dd5821c16
Abdulhakim <Abdulthe383@gmail.com>
2025-02-02 23:13:33 -0500

    Task 3 Inventory Depletion

--- cc_4.js
+++ cc_4.js
@@ -10,3 +10,10 @@ let totalSales = 0;
 for (let i = 0; i < sales.length; i++)
 {totalSales += sales[i]}
 console.log ('Total Sales: $$ {totalSales}');
+
+// Task 3 Inventory Depletion While Loop //
+let stock = 10;
+while (stock > 0) {
+    console.log(`Stock remaining: ${stock}`); 
+stock--; }
+console.log("Depletion");
