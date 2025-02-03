996dd5475c35a734424d8605c82cd40104f92002
Abdulhakim <Abdulthe383@gmail.com>
2025-02-02 23:10:36 -0500

    Task 1 Customer Discounts

---
+++ cc_4.js
@@ -0,0 +1,6 @@
+// Task 1 Customer Discounts If Statements//
+let purchaseAmount = 200;
+let finalAmont = purchaseAmount;
+if (purchaseAmount> 100) { finalAmount = purchaseAmount*.9};
+console.log ("discount applied");
+
